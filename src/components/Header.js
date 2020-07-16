cdc6701959916c8ea5c9946f940eb5e8a88511b5
Berkin <berkintsn@gmail.com>
2020-07-16 03:41:05 +0300

    Header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 function Header() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-transparent justify-content-between">
-            <a class="navbar-brand" href="#">Berkin Tosun</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-transparent justify-content-between">
+            <a className="navbar-brand" href="#">Berkin Tosun</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02" style={{flexGrow:0}}>
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">About</a>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02" style={{flexGrow:0}}>
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#about">About</a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Education</a>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#adventure">Adventure</a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Projects</a>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#ideas">Ideas</a>
                     </li>
                 </ul>
             </div>
