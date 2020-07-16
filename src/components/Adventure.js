33a71bf5e42e58996b13bc9cd3a35b014e6191c7
Berkin <berkintsn@gmail.com>
2020-07-16 03:41:53 +0300

    Converted the education section to full adventure one

---
+++ src/components/Adventure.js
@@ -0,0 +1,115 @@
+import React from 'react';
+
+function Adventure() {
+  return (
+    <div className="d-flex flex-row justify-content-center mb10v" id="adventure">
+      <div className="titleContainer">
+        <h2 className="title">My Little Adventure!</h2>
+      </div>
+
+      <div className="timelineContainer pt-7">
+        <div className="timeline">
+          <div className="timelineIcon left startIcon"><i class="icofont-runner-alt-1"></i>
+          </div><div className="timelineIcon right"><i class="icofont-learn"></i></div>
+          <h3>Yaşar University</h3>
+          <h4>Bachelors on Computer Engineering</h4>
+          <h5>2014-2018</h5>
+          <p>My adventure started right here as I got accepted to the Computer Engineering bachelor programme.
+            I had a chance to work with great people who are skilled at very specific and different fields. This allowed me to try lots of different things and improved my vision.</p>
+        </div>
+        <div className="timeline">
+          <div className="timelineIcon left"><i class="icofont-flask"></i></div>
+          <h3>Yaşar University</h3>
+          <h4>Undergraduate Research Assistant</h4>
+          <h5>2015-2016</h5>
+          <p>During my studies at Yaşar University, my curiosity and thirst for the knowledge must have get the attention of my teachers and they invited me to take a test for the assistant position.
+            This position allowed me to work in the university while I am studying my bachelors and this opportunity allowed me to see the <b><i>kitchen side</i></b> of the academic world.</p>
+        </div>
+        <div className="timeline">
+          <div className="timelineIcon right"><i class="icofont-terminal"></i></div>
+          <h3>Sole PR &amp; Agency</h3>
+          <h4>Software Engineer Intern</h4>
+          <h5>Fall 2017</h5>
+          <p>During this time, my curiosity about web development peaked at that time and I went to Sole just to learn the current state of web development.
+            I learned React and had a chance to mess with React-Native and this demonstrated the endless possibilities of the web development.
+          </p>
+        </div>
+        <div className="timeline">
+          <div className="timelineIcon left"><i class="icofont-dashboard-web"></i></div>
+          <h3>Univerlist</h3>
+          <h4>Software Engineer</h4>
+          <h5>Jan 2017 - Jan 2018</h5>
+          <p>Univerlist was my first startup experience and I assure you, that was an intense experience... I had to work on so many different fields and had to learn so much new stuff.
+            This startup was harsh reality of the actual work-life and this intensive requirements allowed me to reach my best performance and showed me that I can do so many things that I would believe that I couldn't.
+          </p>
+          <p>We developed a university search engine with lots of detailed information about each university in Turkey. I write dozens of web crawlers that updates the information about each university on hundreds of different sources.
+            Also, I developed a special socring algorithm to be able to score universities by using lots of non-biased sources in order to reduce the subjectivity in the scoring system. As this was a startup company, I had to work on Frontend and IOS development as well.
+            This position allowed me to develop myself at a very fast learning pace and I am glad to see that I can handle this kind of pressure in my working life.
+          </p>
+        </div>
+        <div className="timeline">
+          <div className="timelineIcon right"><i class="icofont-computer"></i></div>
+          <h3>Yaşar University</h3>
+          <h4>Software Engineer</h4>
+          <h5>Oct 2017 - Feb 2018</h5>
+          <p>While I work at Univerlist, I wanted to learn Mac environment and I needed an extra cash to buy a Mac just to be able to learn it.
+            In the mean time, Yaşar university opened a part-time position on their IT department and as soon as I see the opportunity, I jumped on it!
+            This job allowed me to experience a corporate work and that was totally different than usual startup workflow. I had to get an approval for every update and my permission to update/change the actual product, was limited.
+          </p>
+          <p>
+            Even so, I had the chance of working with an awesome director Gökhan Panayır, who is a very sweet person also a very passionate one!
+             We developed a private authentication system for internal usage and a face recognition system for the security. We also worked on Frontend of the dashboard and studied the user experience to improve our user interface.
+</p>
+        </div>
+        <div className="timeline">
+          <div className="timelineIcon left"><i class="icofont-university"></i></div>
+          <h3>University Of Tartu</h3>
+          <h4>M.S.c in Software Engineering</h4>
+          <h5>2018-2020</h5>
+          <p>While working at Univerlist, I saw the beautiful country called Estonia and the countless opportunities of University of Tartu.
+            I immediately felt in love with their industrial master's programme. I applied in my last year of bachelors and got accepted with tuitionwaiver!
+          </p>
+        </div>
+        <div className="timeline">
+          <div className="timelineIcon right"><i class="icofont-beaker"></i></div>
+          <h3>IVCV Research Lab</h3>
+          <h4>Research Assistant</h4>
+          <h5>Jan 2019 - Jun 2020</h5>
+          <p>During my studies at University of Tartu, I continued to pursue for more opportunities and I came across to the IVCV research group.
+             As I saw their awesome work and lovely team, I wanted to join and develop my artificial intelligence and deep learning knowledge</p>
+             <p>We developed of dozens of deep learning models by using state of the art technologies and model structures. I managed to develop a model that matches users with job advertisements and vice versa.
+               I had a chance to work with lots of interesting models and neural networks such as GAN and GNN.
+             </p>
+        </div>
+        <div className="timeline">
+          <div className="timelineIcon left"><i class="icofont-unique-idea"></i></div>
+          <h3>Batı Lastik</h3>
+          <h4>Software Engineer Intern</h4>
+          <h5>Jun 2019 - Sep 2019</h5>
+          <p>As I learn more about the business side of the software development, I wanted to practice it by applying it to an actual case.
+             I did a business analysis for the company and find the bottleneck points of their business and improved the efficiency on those areas.
+             Also, created a business application with React Native for internal usage. It provides a statistical information about the performance of each individual employee and individual sale performance of each department.
+             I used NodeJS and MongoDB to allow their frontend team to work on backend side as well so that they can continue to develop the needed features.
+          </p>
+        </div>
+        <div className="timeline">
+          <div className="timelineIcon right"><i class="icofont-code"></i></div>
+          <h3>Playtech</h3>
+          <h4>Java Developer</h4>
+          <h5> Since March 2020</h5>
+          <p>There was an event called 'Delta Career Day' and in this event, I checked countless companies and get in touch with them but there was one specific company that got my interest.
+             You guessed it right! It was Playtech that got my interest because they bring a game device which uses computer vision to bring the tower defense into a physical form. When I asked about it, they just said that it was a fun project made by Playtech team.
+             After I heard that, I applied for this position :)
+          </p>
+          <p>When I joined, company changed its working style because of coronavirus and I missed so many trainings about the internal systems and product of Playtech but still I manage to adapt to it from remotely and this improved my work style flexibility.
+             I am currently working in Automation team and we are automizing the testings for the Playtech products. I have a wonderful team and they helped me every time when I had questions.
+             If you guys read this, then you guys owe me a coffee! :)
+          </p>
+          <div className="timelineIcon right"><i class="icofont-runner-alt-2"></i></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Adventure;
