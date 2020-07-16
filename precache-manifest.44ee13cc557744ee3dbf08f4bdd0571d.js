177d378a93fcba357a1978ab4886cbca75b2acf6
Berkin <berkintsn@gmail.com>
2020-07-16 04:53:37 +0300

    Updates

---
+++ precache-manifest.44ee13cc557744ee3dbf08f4bdd0571d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1917b8e013101973beb9b9bfe7a2bcf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d8ead7a3f086506b0a",
+    "url": "/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "ff4e33cb78a18054e4a2",
+    "url": "/static/css/main.5f9be82f.chunk.css"
+  },
+  {
+    "revision": "d6d8ead7a3f086506b0a",
+    "url": "/static/js/2.a5acec8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5acec8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4e33cb78a18054e4a2",
+    "url": "/static/js/main.96361eb0.chunk.js"
+  },
+  {
+    "revision": "7d060641719bf9cbf423",
+    "url": "/static/js/runtime-main.e2a44ca5.js"
+  },
+  {
+    "revision": "2fa277552b620e8275a07a442ea8cd0f",
+    "url": "/static/media/instagram2.2fa27755.png"
+  },
+  {
+    "revision": "54dcaaaa78d915798873ffb29cdbc3f9",
+    "url": "/static/media/linkedin3.54dcaaaa.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3e0c21e20065ee8423d747e4454f3e13",
+    "url": "/static/media/profile2.3e0c21e2.jpg"
+  },
+  {
+    "revision": "8b55d482229579d5c4e9ea18cbd6fc27",
+    "url": "/static/media/twitter.8b55d482.png"
+  }
+]);
